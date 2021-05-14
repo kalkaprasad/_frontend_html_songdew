7015245bcd239d885371c9bba165e6741c69aa3f
kalkaprasad <52508495+kalkaprasad@users.noreply.github.com>
2021-05-14 14:16:02 +0530

    update

---
+++ Js/popdropdown.js
@@ -0,0 +1,35 @@
+function pop() {
+    document.getElementById("mypop").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('#droppop')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content-pop");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+
+    if (!event.target.matches('#popu')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content-popular");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+
+  function pupular() {
+    document.getElementById("mypopu").classList.toggle("show");
+  }
+  
+  
---
+++ Js/slider.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.lang-btn1').owlCarousel({
+        margin:10,
+        autoWidth:true,
+        nav:false,
+        dots:false,
+        // responsive:{
+        //     0:{
+        //         items:1
+        //     },
+        //     600:{
+        //         items:3
+        //     },
+        //     1000:{
+        //         items:5
+        //     }
+        // }
+    })
+})
